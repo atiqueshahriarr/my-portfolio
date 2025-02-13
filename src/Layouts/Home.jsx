8e9d48b9b1e309b708e1a955fac89dc5b21bdd18
atique-shahriar <satique06@gmail.com>
2025-02-14 00:13:59 +0600

    Update banner section

--- src/Layouts/Home.jsx
+++ src/Layouts/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       <div className="hero-overlay bg-opacity-5 absolute inset-0"></div>
       <div className="text-center relative z-10 flex flex-col items-center">
         <h1 className="mb-5 text-3xl md:text-5xl font-bold text-black leading-normal md:leading-normal">
-          Hey, I&apos;m Atique Shahriar, <br /> I&apos;m <span className="text-[#7f39ee]">{text}</span>
+          Hey, I&apos;m Md. Atique Shahriar, <br /> I&apos;m <span className="text-[#7f39ee]">{text}</span>
           <Cursor
             cursorStyle="_"
             cursorColor="#7f39ee"
@@ -33,14 +33,13 @@ const Home = () => {
         <p className="mb-5"></p>
         <div className="flex gap-6">
           <a
-            href="./Web_Developer_Resume_of_Md. Atique Shahriar Resume_2.pdf"
+            href="./Web_Developer_Resume_of_Md. Atique Shahriar Resume.pdf"
             download
             className="btndesign flex items-center gap-2 hover:w-36 justify-center w-32 h-12 relative text-white font-semibold hover:bg-white bg-black hover:text-black">
             Resume <IoMdCodeDownload className="text-2xl" />
           </a>
           <a
-            href="./Web_Developer_Resume_of_Md. Atique Shahriar Resume_2.pdf"
-            download
+            href="#projects"
             className="btndesign flex items-center justify-center gap-2 hover:w-36 w-32 h-12 relative text-white font-semibold hover:bg-white bg-black hover:text-black">
             Projects <IoCodeSlash className="text-xl" />
           </a>
@@ -49,38 +48,45 @@ const Home = () => {
           <div className="hidden md:flex  gap-4">
             <div className="btnAnimation w-32 h-12 flex items-center justify-center">
               <a
-                href="#"
+                href="https://www.facebook.com/atiqueshahriarr"
                 className="z-10 gap-2 flex items-center justify-center">
                 <FaFacebook className="text-2xl" /> Facebook
               </a>
             </div>
             <div className="btnAnimation w-32 h-12 flex items-center justify-center">
               <a
-                href="#"
+                href="https://www.linkedin.com/in/atiqueshahriarr/"
                 className="z-10 gap-2 flex items-center justify-center">
                 <FaLinkedin className="text-2xl" /> LinkedIn
               </a>
             </div>
             <div className="btnAnimation w-32 h-12 flex items-center justify-center">
               <a
-                href="#"
+                href="https://github.com/atiqueshahriarr"
                 className="z-10 gap-2 flex items-center justify-center">
                 <FaGithub className="text-2xl" /> Github
+              </a>
+            </div>
+            <div className="btnAnimation w-32 h-12 flex items-center justify-center">
+              <a
+                href="https://wa.me/+8801311330653"
+                className="z-10 gap-2 flex items-center justify-center">
+                <FaWhatsapp className="text-2xl" /> Whatsapp
               </a>
             </div>
           </div>
 
           <div className="flex gap-4 text-2xl md:hidden ">
-            <a href="#">
+            <a href="https://www.facebook.com/atiqueshahriarr">
               <FaFacebook />
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/atiqueshahriarr/">
               <FaLinkedin />
             </a>
-            <a href="#">
+            <a href="https://github.com/atiqueshahriarr">
               <FaGithub />
             </a>
-            <a href="#">
+            <a href="https://wa.me/+8801311330653">
               <FaWhatsapp />
             </a>
           </div>
